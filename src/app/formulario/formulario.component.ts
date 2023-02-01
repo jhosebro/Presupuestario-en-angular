9ab9f3ffce1cffccd8b0618af0890c010a7fe0fd
Jhoan Rojas <jhosebro2108@gmail.com>
2023-02-01 18:00:14 -0500

    Se finaliza la aplicacion de presupuesto

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Egreso } from '../egreso/egreso.model';
+import { EgresoServicio } from '../egreso/egreso.servicio';
+import { Ingreso } from '../ingreso/ingreso.model';
+import { IngresoServicio } from '../ingreso/ingreso.servicio';
 
 @Component({
   selector: 'app-formulario',
@@ -7,4 +11,23 @@ import { Component } from '@angular/core';
 })
 export class FormularioComponent {
 
+  tipo:string = "ingresoOperacion"
+  descripcionInput:string = "";
+  valorInput:number = 0;
+
+  constructor(private ingresoServicio: IngresoServicio,
+    private egresoServicio: EgresoServicio){}
+
+  tipoOperacion(evento: any){
+    this.tipo = evento.target.value;
+  }
+
+  agregarValor(){
+    if(this.tipo === "ingresoOperacion"){
+      this.ingresoServicio.ingresos.push(new Ingreso(this.descripcionInput, this.valorInput))
+    }
+    else{
+      this.egresoServicio.egresos.push(new Egreso(this.descripcionInput, this.valorInput))
+    }
+  }
 }
